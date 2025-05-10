f82a14e7657e725d6c50de0479dfde097999a3bd
Flávio Crispin <47084776+flaviocrispin@users.noreply.github.com>
2025-05-09 21:55:47 -0300

    aula 3

--- aula.py
+++ aula.py
@@ -1,19 +1,8 @@
 import os
 os.system('cls' if os.name == 'nt' else 'clear')
-"""
-4.Dado o dicionário:
-frutas = {"maçã": 3, "banana": 5, "laranja": 2}
-Escreva um código que:
-- Peça ao usuário o nome de uma fruta.
-- Verifique se a fruta está no dicionário.
-- Se estiver, mostre quantas unidades têm.
-Caso contrário, diga que a fruta não está disponível.
-"""
 
-aluno = {'nome':'Mario', 'notas':[7, 9, 5, 6], 'presencas':0.8}
+gerador_impares = (x for x in range(20) if x % 2 == 1)
 
-chaves = list(aluno.keys())
-valores = list(aluno.values())
+#lista_impares = list(gerador_impares)
 
-print('Chaves: ', chaves)
-print('Valores:', valores)
+print(gerador_impares)
